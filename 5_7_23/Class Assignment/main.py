5fdb1fd4a18a06c320c499eece4d0b1813f4d37f
Abdur Rahman Bilal <aramb@aramservices.com>
2023-05-14 10:08:13 -0400

    Create main.py

---
+++ 5_7_23/Class Assignment/main.py
@@ -0,0 +1,5 @@
+def Muslim(name):
+    print("As salaamu alaykum wa rahmatullahi wa barakaatuhu, ",name, ". May Allah forgive ",name, "and have mercy on ",name, " and grant the Jannah to ",name, ". Aameen.")
+
+
+Muslim(input("Enter your name with no spaces: "))
