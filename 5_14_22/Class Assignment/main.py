303f2d3784b7b42d744324150cd9cc28c6a219ae
Abdur Rahman Bilal <aramb@aramservices.com>
2023-05-14 10:38:09 -0400

    new file:   5_14_22/Class Assignment/main.py

---
+++ 5_14_22/Class Assignment/main.py
@@ -0,0 +1,6 @@
+def Coding_Classmates(classmate1, classmate2, classmate3, classmate4, classmate5):
+    classmates = [classmate1, classmate2, classmate3, classmate4, classmate5]
+    print("classmates are: \n", classmates[0],"\n", classmates[1], "\n", classmates[2], "\n", classmates[3], "\n", classmates[4])
+
+
+Coding_Classmates(classmate1="Muath", classmate2="Abdur-Rahman Bilal", classmate3="Nasir", classmate4="Abdullah", classmate5="Hassan")
